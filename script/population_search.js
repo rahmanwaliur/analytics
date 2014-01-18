55e6f72738577e0cebf4541c5e4172ef89572b48
smsohan <sohan39@gmail.com>
2014-01-18 11:42:13 -0700

    Showing yearly community population

--- script/population_search.js
+++ script/population_search.js
@@ -13,6 +13,7 @@ window.search_results_template = Handlebars.compile($('#search-result-template')
 var show_search_results = function(keyword, results_view){
   var communities = population_search(keyword);
   results_view.empty();
+  $('#community-population').empty();
 
   _.each(communities, function(community){
     results_view.append(window.search_results_template({community: community}) );
@@ -25,4 +26,22 @@ var search = function(event){
   var results_view = $('#search-results');
 
   show_search_results(keyword, results_view);
+};
+
+window.population_template = Handlebars.compile($('#population-template').html());
+var show_community = function(event){
+  var community = $(event.target).data('community');
+  var community_population = window.population_data[community];
+
+  $('#search-results').empty();
+  $('#community-population').empty();
+
+  var yearly_data = [];
+
+  _.each(_.keys(community_population), function(year){
+    yearly_data.push({year: year, population: community_population[year].population});
+  });
+
+  $('#community-population').append(window.population_template({community: community,yearly_data: yearly_data}));
+
 };
