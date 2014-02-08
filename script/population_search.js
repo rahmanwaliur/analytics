3bb4368f17638afd45c4d7ca5eb81d7449752edd
smsohan <sohan39@gmail.com>
2014-02-08 12:18:18 -0700

    shows population side by side fro two communities

--- script/population_search.js
+++ script/population_search.js
@@ -11,39 +11,48 @@ var population_search = function(population_data, keyword){
 
 window.search_results_template = Handlebars.compile($('#search-result-template').html());
 
-var show_search_results = function(keyword, results_view){
+var show_search_results = function(keyword, results_view, community_id){
   var communities = population_search(window.population_data, keyword);
   results_view.empty();
   $('#community').empty();
 
   _.each(communities, function(community){
-    results_view.append(window.search_results_template({community: community}) );
+    results_view.append(window.search_results_template({community: community, community_id: community_id}) );
   });
+
+  results_view.show().dropdown("toggle")
+  results_view.find('.community-link').on("click", show_community);
 };
 
 var search = function(event){
   event.preventDefault();
-  var keyword = $('#search').val();
-  var results_view = $('#search-results');
+  var keyword = $(event.target).val();
+  var community_id = $(event.target).data("community");
 
-  show_search_results(keyword, results_view);
+  var results_view = $('#search-results-' + community_id);
+
+  show_search_results(keyword, results_view, community_id);
 };
 
 var show_community = function(event){
   var community = $(event.target).data('community');
+  var community_id = $(event.target).data('community-id');
 
-  $('.community').html(community);
+  $('#community-' + community_id).html(community);
 
-  $('#search-results').empty();
-  show_population(community);
-  show_employment(community);
+  $('#search-results-' + community_id).empty().hide();
+
+  show_population(community, community_id);
+  // show_employment(community);
 };
 
 window.population_template = Handlebars.compile($('#population-template').html());
-var show_population = function(community){
+var show_population = function(community, community_id){
   var community_population = window.population_data[community];
 
-  $('#community-population').empty();
+  var population_container =$('#community-population-' + community_id);
+
+  population_container.empty();
 
   var yearly_data = [];
 
@@ -58,7 +67,7 @@ var show_population = function(community){
     yearly_data.push({year: year, population_chunks: chunks, population: $.number(population)});
   });
 
-  $('#community-population').append(window.population_template({community: community,yearly_data: yearly_data}));
+  population_container.append(window.population_template({community: community,yearly_data: yearly_data}));
 };
 
 window.employment_template = Handlebars.compile($('#employment-template').html());
