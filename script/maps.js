4eb0e1614b6eb2d1b3f0f8f09cb6a3d58ffde613
smsohan <sohan39@gmail.com>
2014-02-10 12:15:39 -0700

    maps correcyed

--- script/maps.js
+++ script/maps.js
@@ -1,5 +1,5 @@
 var show_map = function(community, community_id) {
-  var community_code = window.kml_data['community'];
+  var community_code = window.kml_data[community];
 
   var calgary = new google.maps.LatLng(51.0500,-114.0667);
   var mapOptions = {
@@ -10,7 +10,7 @@ var show_map = function(community, community_id) {
   var map = new google.maps.Map(document.getElementById('map-canvas-' + community_id), mapOptions);
 
   var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://smsohan.com/analytics/data/' + community_code + ".kml";
+    url: 'http://smsohan.com/analytics/data/kmls/' + community_code + ".kml"
   });
   ctaLayer.setMap(map);
 };
