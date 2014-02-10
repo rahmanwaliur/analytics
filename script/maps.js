90e32b786da2477d9a9e19c70282e23446a1e02a
smsohan <sohan39@gmail.com>
2014-02-10 11:20:05 -0700

    expermenting with maps

--- script/maps.js
+++ script/maps.js
@@ -1,14 +1,15 @@
 var show_map = function() {
-  var chicago = new google.maps.LatLng(41.875696,-87.624207);
+  var calgary = new google.maps.LatLng(51.0500,-114.0667);
   var mapOptions = {
     zoom: 11,
-    center: chicago
+    center: calgary
   }
 
   var map = new google.maps.Map(document.getElementById('map-canvas-1'), mapOptions);
 
   var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
+    // url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
+    url: 'https://raw2.github.com/smsohan/analytics/master/data/sample.kml'
   });
   ctaLayer.setMap(map);
 };
