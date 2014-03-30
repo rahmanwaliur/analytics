e04b221de19896b67d9d73dd5042fd1200219b41
smsohan <sohan39@gmail.com>
2014-03-30 13:21:17 -0600

    more specs added

--- spec/conf.js
+++ spec/conf.js
@@ -10,7 +10,7 @@ exports.config = {
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['match_page_spec.js'],
+  specs: ['match_page_spec.js', 'compare_page_spec.rb'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
