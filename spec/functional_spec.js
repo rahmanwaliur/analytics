0aab388d642d3d2bacf3586e20e49a1775b074bc
smsohan <sohan39@gmail.com>
2014-03-29 18:28:31 -0600

    refactored a page object

--- spec/functional_spec.js
+++ spec/functional_spec.js
@@ -1,42 +1,63 @@
+var MatchPage = (function(){
+
+  var MatchPage = function(){
+
+    this.get = function(){
+      browser.get('http://localhost:8000');
+    }
+
+    this.selected_income = function(){
+      return element(by.selectedOption('income')).getText();
+    }
+
+    this.select_income = function(value){
+      element(by.model('income')).$('option[value="' + value + '"]').click();
+    }
+
+  };
+
+
+  return MatchPage;
+})();
+
+
 describe('match page', function() {
-  it('sets income to 15000 when user selects under 30K', function() {
-    browser.get('http://localhost:8000');
 
-    element(by.model('income')).$('option[value="15000"]').click();
+  var match_page = null;
 
-    expect(element(by.selectedOption('income')).getText()).toEqual('Less than $30,000')
+  beforeEach(function(){
+    match_page = new MatchPage();
+    match_page.get();
   });
 
-  it('sets income to 45000 when user selects 30-60K', function() {
-    browser.get('http://localhost:8000');
+  describe('income select', function(){
 
-    element(by.model('income')).$('option[value="45000"]').click();
+    it('sets income to 15000 when user selects under 30K', function() {
+      match_page.select_income(15000);
+      expect(match_page.selected_income()).toEqual('Less than $30,000');
+    });
 
-    expect(element(by.selectedOption('income')).getText()).toEqual('$30,000 - $60,000')
+    it('sets income to 45000 when user selects 30-60K', function() {
+      match_page.select_income(45000);
+      expect(match_page.selected_income()).toEqual('$30,000 - $60,000');
+    });
+
+    it('sets income to 75000 when user selects 60-90K', function() {
+      match_page.select_income(75000);
+      expect(match_page.selected_income()).toEqual('$60,000 - $90,000');
+    });
+
+    it('sets income to 105000 when user selects 90-120K', function() {
+      match_page.select_income(105000);
+      expect(match_page.selected_income()).toEqual('$90,000 - $120,000');
+    });
+
+    it('sets income to 130000 when user selects over 120K', function() {
+      match_page.select_income(130000);
+      expect(match_page.selected_income()).toEqual('Above $120,000');
+    });
+
   });
 
-  it('sets income to 75000 when user selects 60-90K', function() {
-    browser.get('http://localhost:8000');
-
-    element(by.model('income')).$('option[value="75000"]').click();
-
-    expect(element(by.selectedOption('income')).getText()).toEqual('$60,000 - $90,000')
-  });
-
-  it('sets income to 105000 when user selects 90-120K', function() {
-    browser.get('http://localhost:8000');
-
-    element(by.model('income')).$('option[value="105000"]').click();
-
-    expect(element(by.selectedOption('income')).getText()).toEqual('$90,000 - $120,000')
-  });
-
-  it('sets income to 130000 when user selects over 120K', function() {
-    browser.get('http://localhost:8000');
-
-    element(by.model('income')).$('option[value="130000"]').click();
-
-    expect(element(by.selectedOption('income')).getText()).toEqual('Above $120,000')
-  });
 
 });
